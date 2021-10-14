8b28ee3143808b066df4a42348b84ea56f9a2480
nodachan <72251931+ytnd0928@users.noreply.github.com>
2021-10-14 18:39:34 +0900

    add

--- api_tureru/wsgi.py
+++ api_tureru/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for api_insta project.
+WSGI config for api_tureru_tureru project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
@@ -9,6 +9,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_insta.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api_turerutureru.settings')
 
 application = get_wsgi_application()
