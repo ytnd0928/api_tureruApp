9a1c627f6e0a7a2ac390737227d23c50dca6c732
nodachan <72251931+ytnd0928@users.noreply.github.com>
2021-11-11 05:01:26 +0900

    add

--- api_insta/settings.py
+++ api_insta/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '93sz7=ro#_8a(_(wk)e2z5l1q71%8x#djos2gm&el68b9gp98-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
