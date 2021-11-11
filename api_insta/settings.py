848709cedfa5a5c4660a7ce2a26fab5bb83c8fe1
nodachan <72251931+ytnd0928@users.noreply.github.com>
2021-11-12 07:59:53 +0900

    add

--- api_insta/settings.py
+++ api_insta/settings.py
@@ -14,18 +14,9 @@ from datetime import timedelta
 
 
 
-import dj_database_url
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'noda',
-        'USER': 'noda',
-        'PASSWORD': 'Noda0928',
-        'HOST': 'host',
-        'PORT': '',
-    }
-}
+
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
