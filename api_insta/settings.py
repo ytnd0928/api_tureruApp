1df22e19d181935d5e047093a067dd7e4a076eda
nodachan <72251931+ytnd0928@users.noreply.github.com>
2021-11-12 08:06:19 +0900

    add

--- api_insta/settings.py
+++ api_insta/settings.py
@@ -37,18 +37,18 @@ ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 # Application definition
 
-INSTALLED_APPS = [
+INSTALLED_APPS = {
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'djoser',
+    'rest_framework',
     'api.apps.ApiConfig',
     'corsheaders',
-]
+}
 
 MIDDLEWARE = [
 
