704fe1608f1cd02d68c392b99831ac03c93d5119
nodachan <72251931+ytnd0928@users.noreply.github.com>
2021-12-11 15:42:31 +0900

    Debug=false

--- api_insta/settings.py
+++ api_insta/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
