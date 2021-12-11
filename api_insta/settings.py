1cdc725e28b99aad9abc396815d13cee61e08270
nodachan <72251931+ytnd0928@users.noreply.github.com>
2021-12-11 15:53:58 +0900

    add whitenise

--- api_insta/settings.py
+++ api_insta/settings.py
@@ -43,9 +43,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-
-
-
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -54,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 CORS_ORIGIN_WHITELIST = [
