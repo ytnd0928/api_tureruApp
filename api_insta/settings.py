5441ed271ca35f0e8329eaf7f9216b5e7ce9b77a
nodachan <72251931+ytnd0928@users.noreply.github.com>
2021-12-11 16:00:31 +0900

    add

--- api_insta/settings.py
+++ api_insta/settings.py
@@ -23,6 +23,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+
+if not DEBUG:
+    SECRET_KEY = os.environ['SECRET_KEY']
+    import django_heroku
+    django_heroku.settings(locals())
+
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 ALLOWED_HOSTS = ['*']
 
